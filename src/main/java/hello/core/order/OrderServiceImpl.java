3b1de359f8438bbea2923f509da858bee6f09dd8
jaemoon <woans5970@naver.com>
2023-12-25 15:58:23 +0900

    section 7: 조회 빈이 2개 이상 - 문제

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -10,17 +10,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-//    @Autowired
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
@@ -31,7 +30,7 @@ public class OrderServiceImpl implements OrderService {
     }
 
     // 테스트 용도
-//    public MemberRepository getMemberRepository() {
-//        return memberRepository;
-//    }
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
